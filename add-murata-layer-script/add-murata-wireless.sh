d364e5a12c445699b95b96ef8bb8649a6dfc2f00
bchen-murata <bill.chen@murata.com>
2019-05-06 01:18:46 -0500

    update the conf files to include uSD-M2 adapter's dtb files into the image

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -1,5 +1,11 @@
 cp ./sources/meta-murata-wireless/freescale/imx6ulevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6ull14x14evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx6dlsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx6qpsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx6qsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx6slevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx6sxsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx6ull9x9evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 EULA=$EULA DISTRO=$DISTRO MACHINE=$MACHINE . ./sources/meta-fsl-bsp-release/imx/tools/fsl-setup-release.sh -b $@
 
 echo "INTERNAL_MIRROR = \"http://localhost\"" >> conf/local.conf
