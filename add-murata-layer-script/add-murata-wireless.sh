c5a19847e9d5fc5cb60af1f0a07781a7707b7432
jameel-kareem <jkareem@murata.com>
2020-10-26 16:09:45 -0400

    Add new dtb files of 6ul, 6ull, 8m-mini and 8m-nano to configuration files

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -1,11 +1,15 @@
 cp ./sources/meta-murata-wireless/freescale/imx6ulevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6ull14x14evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
-cp ./sources/meta-murata-wireless/freescale/imx6ull9x9evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6slevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6sxsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6qsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6qpsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6dlsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx8_all.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx8mmevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx8mmlpddr4evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx8mnevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/imx8mnlpddr4evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 
 #EULA=$EULA DISTRO=$DISTRO MACHINE=$MACHINE . ./sources/meta-fsl-bsp-release/imx/tools/fsl-setup-release.sh -b $@
 . ./setup-environment $@
@@ -18,6 +22,17 @@ echo "CORRECTION: Murata modified the following files"
 echo "  - bblayers.conf present in <BUILD_DIR>/conf"
 echo "  - local.conf present in <BUILD_DIR>/conf"
 echo "  - imx6ulevk.conf present in sources/meta-freescale/conf/machine"
+echo "  - imx6ull14x14evk.conf present in ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx6slevk.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx6sxsabresd.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx6qsabresd.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx6qpsabresd.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx6dlsabresd.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx8_all.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx8mmevk.conf.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx8mmlpddr4evk.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx8mnevk.conf.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx8mnlpddr4evk.conf.conf.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
 echo ""
 echo "Murata-Wireless setup complete. Create an image with:"
 echo "    $ bitbake fsl-image-validation-imx"
