d6a3b802bf4b37dd13260ff80d5856f72e8e42f2
jameel-kareem <jkareem@murata.com>
2020-12-02 22:40:54 -0500

    Add wireless-regdb recipe for populating regulatory.db file in /lib/firmware

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -12,7 +12,7 @@ cp ./sources/meta-murata-wireless/freescale/imx8mmevk.conf ./sources/meta-fsl-bs
 . ./setup-environment $@
 
 echo "INTERNAL_MIRROR = \"http://localhost\"" >> conf/local.conf
-echo "CORE_IMAGE_EXTRA_INSTALL += \" hostap-conf hostap-utils hostapd backporttool-linux murata-binaries iperf3 bluez5 bluez5-noinst-tools bluez5-obex openobex obexftp glibc-gconv-utf-16 glibc-utils python3\"" >> conf/local.conf
+echo "CORE_IMAGE_EXTRA_INSTALL += \" hostap-conf hostap-utils hostapd backporttool-linux wireless-regdb murata-binaries iperf3 bluez5 bluez5-noinst-tools bluez5-obex openobex obexftp glibc-gconv-utf-16 glibc-utils python3\"" >> conf/local.conf
 echo "IMAGE_INSTALL_remove += \" packagegroup-fsl-tools-gpu-external vkmark\"" >> conf/local.conf
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-murata-wireless \"" >> conf/bblayers.conf
 echo ""
