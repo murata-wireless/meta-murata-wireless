7c97a13aeeaf1d460c1ce0f09fee1bec29b3a0a6
jameel-kareem <jkareem@murata.com>
2020-12-07 17:20:00 -0500

    Resolve Krogoth build faiure

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -5,7 +5,8 @@ cp ./sources/meta-murata-wireless/freescale/imx6qpsabresd.conf ./sources/meta-fs
 cp ./sources/meta-murata-wireless/freescale/imx6qsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6slevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
 cp ./sources/meta-murata-wireless/freescale/imx6sxsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
-cp ./sources/meta-murata-wireless/freescale/imx6ull9x9evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/
+cp ./sources/meta-murata-wireless/freescale/firmware-imx_5.4.bbx ./sources/meta-fsl-arm/recipes-bsp/firmware-imx/firmware-imx_5.4.bb
+
 EULA=$EULA DISTRO=$DISTRO MACHINE=$MACHINE . ./sources/meta-fsl-bsp-release/imx/tools/fsl-setup-release.sh -b $@
 
 echo "INTERNAL_MIRROR = \"http://localhost\"" >> conf/local.conf
@@ -17,7 +18,13 @@ echo ""
 echo "CORRECTION: Murata modified the following files"
 echo "  - bblayers.conf present in <BUILD_DIR>/conf"
 echo "  - local.conf present in <BUILD_DIR>/conf"
-echo "  - imx6ulevk.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine"
+echo "  - imx6ulevk.conf present in sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/"
+echo "  - imx6ull14x14evk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/"
+echo "  - imx6dlsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/"
+echo "  - imx6qpsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/"
+echo "  - imx6qsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/"
+echo "  - imx6slevk.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/"
+echo "  - imx6sxsabresd.conf ./sources/meta-fsl-bsp-release/imx/meta-bsp/conf/machine/"
 echo ""
 echo "Murata-Wireless setup complete. Create an image with:"
 echo "    $ bitbake fsl-image-validation-imx"
--- recipes-kernel/linux/linux-imx_4.1.15.bbappend
+++ recipes-kernel/linux/linux-imx_4.1.15.bbappend
@@ -6,6 +6,9 @@ FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}-${PV}:"
 SRC_URI_append = " file://0001-defconfig.patch \
                    file://0002-murata-dts-3.3v.patch"
 
+#SRC_URI_append = " file://0001-defconfig.patch"
+
+
 addtask copy_defconfig_after_patch after do_patch before do_configure
 do_copy_defconfig_after_patch () {
     # copy latest imx_v7_defconfig to use
