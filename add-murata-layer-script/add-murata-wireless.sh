f417351b40d58ff52274629dfbd8e6aa08747119
jameel-kareem <jkareem@murata.com>
2021-01-07 01:27:31 -0500

    Made the switch for machine type with imx6ull14x14evk

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -30,3 +30,34 @@ echo "Murata-Wireless setup complete. Create an image with:"
 echo "    $ bitbake fsl-image-validation-imx"
 echo ""
 
+# Start parsing first line from local.conf for fetching MACHINE type
+COUNTER=0
+input_file_path="conf/local.conf"
+IMX6ULL='imx6ull14x14evk'
+while IFS= read -r LATEST_VER
+do
+	COUNTER=$[$COUNTER +1]
+	if [ "$COUNTER" = "1" ] ; then
+	break
+	fi
+done < "$input_file_path"
+
+IFS== read FIRST_LINE MACHINE_TEXT LATEST_VER <<< $LATEST_VER
+#echo "1. DEBUG-DEBUG:: First line is machine type:$MACHINE_TEXT"
+
+MACHINE_TEXT=`echo $MACHINE_TEXT | sed 's/^ *$//g'`
+#echo "2. DEBUG-DEBUG:: First line is machine type:$MACHINE_TEXT-$IMX6ULL"
+
+MACHINE_TEXT=`echo $MACHINE_TEXT | sed 's/.$//'`
+#echo "3. DEBUG-DEBUG:: First line is machine type:$MACHINE_TEXT"
+
+MACHINE_TEXT=`echo $MACHINE_TEXT | sed 's/^.//'`
+#echo "4. DEBUG-DEBUG:: First line is machine type:$MACHINE_TEXT"
+
+if [ "$MACHINE_TEXT" == $IMX6ULL ]; then
+#	echo    "Latest:  Success"
+#	pwd
+	cd ..
+	cp ./sources/meta-murata-wireless/freescale/linux-imx_4.1.15.bbx ./sources/meta-fsl-bsp-release/imx/meta-bsp/recipes-kernel/linux/linux-imx_4.1.15.bb
+fi
+
