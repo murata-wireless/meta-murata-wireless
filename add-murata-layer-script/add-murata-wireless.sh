1fea7594e3bf1531494197c1f133de3ed9d825bb
jameel-kareem <jkareem@murata.com>
2021-09-02 16:11:59 -0400

    Add cyw hostapd to the packages

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -18,7 +18,7 @@ cp ./sources/meta-murata-wireless/freescale/imx8mn-lpddr4-evk.conf ./meta-freesc
 . ./setup-environment $@
 
 echo "INTERNAL_MIRROR = \"http://localhost\"" >> conf/local.conf
-echo "CORE_IMAGE_EXTRA_INSTALL += \" hostap-conf hostap-utils hostapd murata-binaries iperf3  kernel-modules-sdio8997 \"" >> conf/local.conf
+echo "CORE_IMAGE_EXTRA_INSTALL += \" hostap-conf hostap-utils hostapd murata-binaries iperf3  kernel-modules-sdio8997 crda wireless-regdb-static cyw-hostapd \"" >> conf/local.conf
 echo "CORE_IMAGE_EXTRA_INSTALL += \" bluez5 bluez5-noinst-tools bluez5-obex openobex obexftp glibc-gconv-utf-16 glibc-utils cyw-supplicant python3\"" >> conf/local.conf
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-murata-wireless \"" >> conf/bblayers.conf
 echo ""
