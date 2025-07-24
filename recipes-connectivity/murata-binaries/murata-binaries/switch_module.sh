a277c6ac9eb38854e2f910984b34dc415044f2ee
Romit Chatterjee <147787673+romit-murata@users.noreply.github.com>
2025-07-24 08:11:16 +0530

    Added kraken support

--- recipes-connectivity/murata-binaries/murata-binaries/switch_module.sh
+++ recipes-connectivity/murata-binaries/murata-binaries/switch_module.sh
@@ -147,8 +147,17 @@ function prepare_for_cypress() {
   2EA-SDIO|2EA-PCIE)
      cp /lib/firmware/brcm/CYW55560A1_001.002.087.0269.0100.FCC.2EA.sAnt.hcd /lib/firmware/brcm/BCM.hcd
     ;;
-  2FY|FY|2GY|GY)
-     cp /lib/firmware/brcm/CYW55500A1_001.002.032.0040.0033.2FY.hcd /lib/firmware/brcm/BCM.hcd
+  2FY|FY)
+     cp /lib/firmware/cypress/cyfmac55500-sdio.trxse /lib/firmware/cypress/cyfmac55500-sdio.trxse
+     cp /lib/firmware/cypress/cyfmac55500-sdio.2FY.txt /lib/firmware/cypress/cyfmac55500-sdio.txt
+     cp /lib/firmware/cypress/cyfmac55500-sdio.2FY.clm_blob /lib/firmware/cypress/cyfmac55500-sdio.clm_blob
+     cp /lib/firmware/brcm/CYW55500A1_001.002.032.0040.0033.FCC.2FY.2GY.hcd /lib/firmware/brcm/BCM.hcd
+    ;;
+  2GY|GY)
+     cp /lib/firmware/cypress/cyfmac55500-sdio.trxse /lib/firmware/cypress/cyfmac55500-sdio.trxse
+     cp /lib/firmware/cypress/cyfmac55500-sdio.2GY.txt /lib/firmware/cypress/cyfmac55500-sdio.txt
+     cp /lib/firmware/cypress/cyfmac55500-sdio.2GY.clm_blob /lib/firmware/cypress/cyfmac55500-sdio.clm_blob
+     cp /lib/firmware/brcm/CYW55500A1_001.002.032.0040.0033.FCC.2FY.2GY.hcd /lib/firmware/brcm/BCM.hcd
     ;;
   esac
 
@@ -177,9 +186,9 @@ function off() {
 
 function switch_to_cypress_sdio() {
   echo ""
-  echo "Setting up for 1DX, 1LV, 1MW, 1WZ, 1YN, 2AE, 2BC, 2BZ, 2EA, 2GF, 2FY (Cypress - SDIO)"
+  echo "Setting up for 1DX, 1LV, 1MW, 1WZ, 1YN, 2AE, 2BC, 2BZ, 2EA, 2GF, 2FY, 2GY (Cypress - SDIO)"
 
-#  if [ $cyw_module == "2EA-SDIO" ] || [ $cyw_module == "2FY" ]; then
+#  if [ $cyw_module == "2EA-SDIO" ] || [ $cyw_module == "2FY" ] || [ $cyw_module == "2GY" ]; then
 #     echofw_setenv fdt_file imx8mm-ea-ucom-kit_${DTB_VER}-2ea.dtb 2>/dev/null
 #     fw_setenv bt_hint cypress_2ea
 #     fw_setenv cmd_custom
@@ -191,7 +200,7 @@ function switch_to_cypress_sdio() {
 #     restore_ko
 #  fi
 
-  # Set sdio_idleclk_disable=1 parameter when loading brcmfmac for 2FY.
+  # Set sdio_idleclk_disable=1 parameter when loading brcmfmac for 2FY/2GY.
   # The file created here is deleted in clean_up function above.
   if [ $cyw_module == "2FY" ] || [ $cyw_module == "2GY" ]; then
      echo "options brcmfmac sdio_idleclk_disable=1" > /etc/modprobe.d/2fy_m2.conf
@@ -264,7 +273,7 @@ function usage() {
   echo ""
   echo "Where:"
   echo "  <module> is one of (case insensitive):"
-  echo "     1DX, 1LV, 1MW, 1YN, 2AE, 2AE-USB, 2BC, 2BC-USB, 1XA, 2BZ, 2GF, 2FY, 2EA-SDIO, 2EA-PCIe"
+  echo "     1DX, 1LV, 1MW, 1YN, 2AE, 2AE-USB, 2BC, 2BC-USB, 1XA, 2BZ, 2GF, 2FY, 2GY, 2EA-SDIO, 2EA-PCIe"
   echo ""
 }
 
@@ -280,7 +289,7 @@ case ${1^^} in
   CYW-PCIE|XA|1XA|2EA-PCIE)
     switch_to_cypress_pcie
     ;;
-  CYW-SDIO|LV|1LV|DX|1DX|MW|1MW|YN|1YN|2AE|2BC|2EA-SDIO|BZ|2BZ|GF|2GF|FY|2FY)
+  CYW-SDIO|LV|1LV|DX|1DX|MW|1MW|YN|1YN|2AE|2BC|2EA-SDIO|BZ|2BZ|GF|2GF|FY|2FY|GY|2GY)
     switch_to_cypress_sdio
     ;;
   AE-USB|2AE-USB)
